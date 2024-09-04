93024dd7a243bf6fa48be9d44c5f3a90cbe23d16
EsterHasianna <esternainggolan780@gmail.com>
2024-09-04 21:27:14 +0700

    sudah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24050-Ester Nainggolan
+// 12S24050 - Ester Nainggolan
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        System.out.println("masukkan nama");
+        nama = input.nextLine();
+    }
+}
